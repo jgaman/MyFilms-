bdce167358f6349439c1ce75fd4de6d3d574ef80
Jean Luc GAMAN <43534237+jgaman@users.noreply.github.com>
2020-07-04 22:11:38 +0200

    Rajout du contexte de connection à la DB dans le fichier startup.cs

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,9 @@ namespace myFilms
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MvcMovieContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MvcMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
