a2851458e224343b6228a8b67ad3d8d90458911c
Jean Luc GAMAN <43534237+jgaman@users.noreply.github.com>
2020-07-04 21:56:00 +0200

    Modification du fichier startups: using des differents modèles

--- Startup.cs
+++ Startup.cs
@@ -7,7 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
 namespace myFilms
 {
     public class Startup
